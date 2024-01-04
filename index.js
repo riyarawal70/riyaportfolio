90f437d9ffaf7c22de535395bdc5ad54296e2ca4
riyarawal70 <147626453+riyarawal70@users.noreply.github.com>
2024-01-04 11:02:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const intersectionCallback = (items) => {
+    console.log(items);
+    for (const item of items) {
+      if (item.isIntersecting) {
+        if (item.target.id == "skills") {
+          item.target.classList.add("techskills");
+        }
+        if (item.target.id == "about") {
+          item.target.classList.add("about-animate");
+        }
+        if (item.target.id == "contact") {
+            item.target.classList.add("about-animate");
+          }
+      }
+    }
+  };
+  
+  const observer = new IntersectionObserver(intersectionCallback);
+  
+  const items = document.getElementsByClassName("animate");
+  for (const item of items) {
+    observer.observe(item);
+  }
